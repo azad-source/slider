747bb12d1b5a148e4cae97a8b9cd6f161e68eebf
Azad MAMEDOV <mamedov.a@skbkontur.ru>
2024-08-18 18:33:28 +0400

    set base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import viteTsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [viteTsconfigPaths()],
+  base: "/slider/",
   build: { chunkSizeWarningLimit: 1000 },
   server: {
     open: true,
