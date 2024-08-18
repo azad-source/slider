2994560cfa5c4a29646d97e3a7e6a7d6e45aa9d4
Azad MAMEDOV <mamedov.a@skbkontur.ru>
2024-08-18 18:45:04 +0400

    add vite-plugin-commonjs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import viteTsconfigPaths from "vite-tsconfig-paths";
+import commonjs from "vite-plugin-commonjs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [viteTsconfigPaths()],
+  plugins: [viteTsconfigPaths(), commonjs()],
   base: "/slider/",
   build: { chunkSizeWarningLimit: 1000 },
   server: {
